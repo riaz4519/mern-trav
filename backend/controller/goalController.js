96b17327bf9403ce6cefd0c9ce3d98b3cb0f7499
riaz4519 <riaz.i3216@gmail.com>
2022-03-27 18:44:40 +0600

    after routing done

---
+++ backend/controller/goalController.js
@@ -0,0 +1,34 @@
+//@desc Get Goals
+//@route GET /api/goals
+//@access Private
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+};
+
+//@desc Set Goals
+//@route POST /api/goals
+//@access Private
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set Goals" });
+};
+
+//@desc update goal
+//@route PUT /api/goals
+//@access Private
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+
+//@desc Delete goal
+//@route Delete /api/goals
+//@access Private
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
