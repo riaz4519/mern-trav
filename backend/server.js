e6c0cadf5dc8a564d8f7f85ce6bc974cb6dcc98b
riaz4519 <riaz.i3216@gmail.com>
2022-03-28 22:25:45 +0600

    after create a model

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
-console.log("Hello world!");
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 //routers
 const goalRoutes = require("./routes/goalRoutes");
 
 const app = express();
 
-app.use("/api/goals",goalRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", goalRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
