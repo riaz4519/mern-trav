a0d9ef3aa4da839cc8795c751f6d506449883f29
riaz4519 <riaz.i3216@gmail.com>
2022-03-29 22:49:34 +0600

    in authentication

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,13 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
-//routers
-const goalRoutes = require("./routes/goalRoutes");
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", goalRoutes);
+app.use("/api/goals", require("./routes/goalRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 
 app.use(errorHandler);
 
